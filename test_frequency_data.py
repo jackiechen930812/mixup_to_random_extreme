1e8a30e382c796e6108b5e2cf48af7ee40a84bff
Jackie Chen <jackichen@nvidia.com>
2022-03-31 13:12:43 +0800

    add HFC test

---
+++ test_frequency_data.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+import torchvision.utils
+from torchvision import models
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import models
+
+from new_dataset import New_Dataset
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(), #255 1
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+saved_model_path = './checkpoint/ResNet18/ckpt.pth_ResNet18_epoch200_none_20220328'
+
+radius = [4, 8, 12, 16, 20, 24, 28]
+for r in radius:
+    # test_low = './data/CIFAR10/test_data_low_' + str(r) + '.npy'
+    test_high = './data/CIFAR10/test_data_high_' + str(r) + '.npy'
+
+
+    # testset = New_Dataset(test_low,'./data/CIFAR10/test_label.npy',transform_test)
+    testset_high_4 = New_Dataset(test_high,'./data/CIFAR10/test_label.npy',transform_test)
+    # testset = testset.__add__(testset_high_4)
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=10,
+    #                                          shuffle=False, num_workers=8)
+
+    testloader = torch.utils.data.DataLoader(testset_high_4, batch_size=10,
+                                             shuffle=False, num_workers=8)
+    checkpoint = torch.load(saved_model_path)
+    net = checkpoint['net']
+    net.eval().cuda()
+
+    # 模型在原测试集上的准确率
+    correct = 0
+    total = 0
+
+    for images, labels in testloader:
+        
+        images = images.cuda()
+        outputs = net(images)
+        
+        _, predicted = torch.max(outputs.data, 1)
+        
+        total += labels.size(0)
+        correct += (predicted == labels.cuda()).sum()
+        
+    print('frequency =',r,', test accuracy: %.2f %%' % (100 * float(correct) / total))
+
--- train.py
+++ train.py
@@ -24,9 +24,6 @@ import Gau_noise
 import mixup as mp
 import cutmix as cx
 import mixup_v2 as mp_v2
-
-from new_dataset import New_Dataset
-
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
@@ -56,56 +53,25 @@ transform_train = transforms.Compose([
 ])
 
 transform_test = transforms.Compose([
-    Gau_noise.AddGaussianNoise(0.0, 8.0, 1.0),
+    # Gau_noise.AddGaussianNoise(0.0, 8.0, 1.0),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-
-use_frequency_data = True
-
-if use_frequency_data:
-    # 加载生成的后缀为4的npy数据
-    trainset = New_Dataset('./data/CIFAR10/train_data_low_4.npy','./data/CIFAR10/train_label.npy',transform_train)
-    trainset_high_4 = New_Dataset('./data/CIFAR10/train_data_high_4.npy','./data/CIFAR10/train_label.npy',transform_train)
-    trainset = trainset.__add__(trainset_high_4)     #将trainset_high_4数据集拼接在trainset后
-
-    radius = [ 8, 12, 16]       #还要加载的其他后缀的数据，后缀可在这个列表中修改，源代码中默认加载4、8、12、16
-
-    for r in radius:
-        train_low = './data/CIFAR10/train_data_low_' + str(r) + '.npy'  #要加载的数据地址
-        train_high = './data/CIFAR10/train_data_high_' + str(r) + '.npy'
-        trainset_low = New_Dataset(train_low,'./data/CIFAR10/train_label.npy',transform_train) #加载
-        trainset = trainset.__add__(trainset_low) #拼接
-        trainset_high = New_Dataset(train_high,'./data/CIFAR10/train_label.npy',transform_train)
-        trainset = trainset.__add__(trainset_high)
-    
-    #testset部分与trainset相对应
-    testset = New_Dataset('./data/CIFAR10/test_data_low_4.npy','./data/CIFAR10/test_label.npy',transform_test)
-    testset_high_4 = New_Dataset('./data/CIFAR10/test_data_high_4.npy','./data/CIFAR10/test_label.npy',transform_test)
-    testset = testset.__add__(testset_high_4) 
-    for r in radius:
-        test_low = './data/CIFAR10/test_data_low_' + str(r) + '.npy'
-        test_high = './data/CIFAR10/test_data_high_' + str(r) + '.npy'
-        testset_low = New_Dataset(test_low,'./data/CIFAR10/test_label.npy',transform_test)
-        testset = testset.__add__(testset_low)
-        testset_high = New_Dataset(test_high,'./data/CIFAR10/test_label.npy',transform_test)
-        testset = testset.__add__(testset_high)    
-else:
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR10(
-        root='./data', train=False, download=True, transform=transform_test)
-
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-        testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
-# 
+# Model
 print('==> Building model..')
 # net = VGG('VGG19')
 # net = ResNet18()
@@ -150,7 +116,7 @@ if device == 'cuda':
 
 if not os.path.isdir('results'):
     os.mkdir('results')
-logname = ('results/log' +  '_' + args.model + '_epoch200_gau_cutmix_'
+logname = ('results/log' +  '_' + args.model + '_epoch200_none_'
            + str(args.seed) + '.csv')
 
 
@@ -175,8 +141,6 @@ def train(epoch):
                                                               args.alpha)
             inputs = inputs.float()
             outputs = net(inputs)
-            targets_a = targets_a.long()
-            targets_b = targets_b.long()
             loss = mp.mixup_criterion(criterion, outputs, targets_a, targets_b, lam)
             train_loss += loss.item()
             _, predicted = outputs.max(1)
@@ -194,11 +158,6 @@ def train(epoch):
             one_third = int(batch_size / 3)
             inputs_v2, targets_a_v2, targets_b_v2, lam_v2 = mp_v2.mixup_data(inputs, targets, args.alpha)
             inputs_v1, targets_a_v1, targets_b_v1, lam_v1 = mp.mixup_data(inputs, targets, args.alpha)
-            targets = targets.long()
-            targets_a_v1 = targets_a_v1.long()
-            targets_a_v2 = targets_a_v2.long()
-            targets_b_v1 = targets_b_v1.long()
-            targets_b_v2 = targets_b_v2.long()
 
             inputs_mix = torch.cat((inputs[:one_third], inputs_v1[one_third:2 * one_third], inputs_v2[2 * one_third:]), 0)
             inputs_mix = inputs_mix.float()
@@ -282,7 +241,7 @@ def test(epoch):
         if not os.path.isdir('checkpoint/ResNet18/'):
             os.mkdir('checkpoint/ResNet18/')
 
-        torch.save(state, './checkpoint/ResNet18/ckpt.pth_' + args.model + '_epoch200_gau_cutmix' + '_'
+        torch.save(state, './checkpoint/ResNet18/ckpt.pth_' + args.model + '_epoch200_none' + '_'
                    + str(args.seed))
         best_acc = acc
     return (test_loss / batch_idx, 100. * correct / total)
