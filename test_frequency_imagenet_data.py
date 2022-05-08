1c1c0ef8653c1bc1352151beb2f774d9fe66d6b0
Jackie Chen <jackichen@nvidia.com>
2022-05-08 11:52:06 +0800

    add HFC for imagenet100

---
+++ test_frequency_imagenet_data.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+import torchvision.utils
+from torchvision import models
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from torch.utils.data.distributed import DistributedSampler
+import models
+import argparse
+
+from new_dataset import New_Dataset
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--local_rank', type=int, default=0)
+
+args = parser.parse_args()
+
+torch.cuda.set_device(args.local_rank)  # 这里设定每一个进程使用的GPU是一定的，即一张gpu一个进程
+device = torch.device('cuda', args.local_rank)
+torch.distributed.init_process_group(backend='nccl') #初始化
+
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(), #255 1
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+saved_model_path = "./checkpoint/ResNet18/ckpt.pth_resnest50_epoch200_matrix_20220502"
+#saved_model_path = "./checkpoint/ResNet18/ckpt.pth_resnest50_epoch200_ori_20220502"
+#saved_model_path = "./checkpoint/ResNet18/ckpt.pth_resnest50_epoch200_cutmix_20220501"
+#saved_model_path = "./checkpoint/ResNet18/ckpt.pth_resnest50_epoch200_none_20220501"
+
+
+#radius = [4, 8, 12, 16, 20, 24, 28]
+radius = [32, 64, 96, 128, 160, 192, 224]
+
+for r in radius:
+    #test_low = './data/test_data_low_' + str(r) + '.npy'
+    test_high = './data/test_data_high_' + str(r) + '.npy'
+
+    #testset = New_Dataset(test_low,'./data/test_label.npy',transform_test)
+    testset_high_4 = New_Dataset(test_high,'./data/test_label.npy',transform_test)
+
+    #testloader = torch.utils.data.DataLoader(testset, batch_size=10,
+    #                                         shuffle=False, num_workers=8)
+
+    testloader = torch.utils.data.DataLoader(testset_high_4, batch_size=10,
+                                              shuffle=False, num_workers=8)
+    checkpoint = torch.load(saved_model_path)
+    net = checkpoint['net']
+    net.eval()
+    # net.eval().cuda()
+
+    # 模型在原测试集上的准确率
+    correct = 0
+    total = 0
+
+    for images, labels in testloader:
+        
+        images = images.cuda()
+        outputs = net(images)
+        
+        _, predicted = torch.max(outputs.data, 1)
+        
+        total += labels.size(0)
+        correct += (predicted == labels.cuda()).sum()
+        
+    print('frequency =',r,', test accuracy: %.2f %%' % (100 * float(correct) / total))
+    
+
